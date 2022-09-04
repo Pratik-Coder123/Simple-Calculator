231a4dc2a309544696d98f0b2389f636cece45a8
Pratik <58485228+Pratik-Coder123@users.noreply.github.com>
2022-09-04 22:23:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+var result=document.getElementById("inputtext")
+let screenvalue="" 
+
+let calculate =(number)=>{
+
+result.value=result.value+number
+
+}
+
+let clr =()=>{
+
+    result.value=""
+}
+
+let del=()=>{
+
+    result.value=result.value.slice(0,-1)
+}
+
+
+let Result=()=>{
+
+    try {
+        result.value=eval(result.value);
+    } catch (error) {
+        // alert("Please provide valid input") ;
+         result.value="Invalid Operation"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var result=document.getElementById("inputtext")
+
+// let calculate=(number)=>{
+// result.value=result.value+number
+// }
+// let Result=()=>{
+// try {
+//     result.value=eval(result.value)
+// } 
+// catch (err) {
+//     alert("Enter the valid input")
+    
+// }
+// }
+// function clr() {
+      
+//     result.value=" ";
+
+// }
+
+// function del() {
+//    result.value=result.value.slice(0,-1) 
+// }
